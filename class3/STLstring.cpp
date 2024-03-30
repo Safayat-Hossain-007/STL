4f43c6ff6dc9b1a85ac00eb234f1e2af7ea65199
Safayat <safayathossain40@gmail.com>
2024-03-31 00:03:54 +0600

    adde

--- class3/STLstring.cpp
+++ class3/STLstring.cpp
@@ -29,12 +29,74 @@ int main(){
 
     //concatenation of two string
 
-    string s0;
-    string s1 = "mike tyson";
-    char c = ' ';
-    string s2 = "jake paul";
-    s0 = s1 + c + s2;
-    cout<<s0;
+    // string s0;
+    // string s1 = "mike tyson";
+    // char c = ' ';
+    // string s2 = "jake paul";
+    // s0 = s1 + c + s2;
+    // cout<<s0;
+    // //clearing a string
+    // s0.clear();
+    // cout<<s0;
+
+    // //copying a string
+    // string s4 = "harold";
+    // string s5 = "jake";
+    // s4 =s5;
+    // cout<<s4<<endl;
+
+    // s4.clear();
+    // cout<<s4.empty();
+
+    //using iterator on vector
+    // vector<int> v ;
+    // int n;
+    // cin>>n;
+    // for(int i=0;i<n;i++){
+    //     int x;
+    //     cin>>x;
+    //     v.push_back(x);
+    // }
+    // vector<int>::iterator it;
+    // for(it = v.begin();it != v.end() ; it++){
+    //     cout<<*it;
+    // }
+
+
+
+    // string s = "safayat hossain";
+    
+    //using iterator to access the element of the string
+    // string::iterator it1;
+    // for(it1 = s.begin(); it1 <= s.end(); it1++){
+    //     cout << *it1;
+    // }
+    // cout<<endl;
+
+    //using for each loop to access the element of the string
+    // string s4 = "safayat hossain";
+    // for(auto p: s4){
+    //     cout<<p;
+    // }
+    // cout<<endl;
+
+    // //comparing two strings
+    // string s1 ="safayat";
+    // string s2 ="hossain";
+    // if(s1==s2){ //O(N)
+    //     cout<<"equal"<<endl;
+    // }
+    // else{
+    //     cout<<"not equal"<<endl;
+    // }
+
+    // //reversing a string
+    // string s1 = "safayat";
+    // string s2 = "hossain";
+    // reverse(s1.begin(),s1.end()); //O(N)
+    // cout<<s1<<" ";
+
+    
 
 
 }
