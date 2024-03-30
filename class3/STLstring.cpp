006839dfb753319b73a476469d4579857965ce55
Safayat <safayathossain40@gmail.com>
2024-03-30 22:28:24 +0600

    added

---
+++ class3/STLstring.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>//it contains all types if headers file
+using namespace std;
+int main(){
+    // //declearing a String
+    // string s;
+    // //con catnation 
+    // cin>>s;
+    // s = s + 'a';
+    // s += s+'b';
+    // s += s+'c';
+    // cout<<s<<" ";
+
+    //initializing a string
+    // string s = "safayat";
+    // s += " ";
+    // s += "hossain";
+    // cout<<s<<" "<<endl;
+
+    //size of the string
+    // cout<<s.length()<<" "<<endl;
+
+    //accessing string index by index
+    //the reason we are able access this index because string saves them as static 
+    // cout<<s[0]<<" "<<s[1]<<" "<<s[2]<<" "<<endl;
+
+    //changing index wise character of the string
+    // s[0] = 'r';
+    // cout<<s<<endl;
+
+    //concatenation of two string
+
+    string s0;
+    string s1 = "mike tyson";
+    char c = ' ';
+    string s2 = "jake paul";
+    s0 = s1 + c + s2;
+    cout<<s0;
+
+
+}
