d15d602a2241ac1f5b11d383a1b67170d4fdb2b3
Safayat Hossain <safayathossain40@gmail.com>
2024-04-11 23:19:32 +0600

    added

--- class2/sortingUniqueElement.cpp
+++ class2/sortingUniqueElement.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    vector<int> v ={2,3,5,5,7,7,1};
+    vector<int> v ={1,1,2,2,3,3};
     sort(v.begin(),v.end());//O(nlog2(n)
     
     int size = unique(v.begin(),v.end()) - v.begin(); //O(n)
